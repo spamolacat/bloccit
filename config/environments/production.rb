377e136658b6bd3c6915a0c7145517ad8a0fcc7b
Matt MacDonald <spamolacat@yahoo.com>
2013-10-20 13:59:09 -0700

    Re-configured app for Prod email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,7 @@ Bloccit::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 # http://spamolacat-bloccit.herokuapp.com
-  config.action_mailer.default_url_options = { :host => 'http://spamolacat-bloccit.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'spamolacat-bloccit.herokuapp.com' }
   
 
 end
