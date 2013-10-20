394f634d6681aa1b9d34ed6f250c21b3945f47c0
Matt MacDonald <spamolacat@yahoo.com>
2013-10-20 13:34:29 -0700

    Configured app for Prod emails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,8 @@ Bloccit::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+# http://spamolacat-bloccit.herokuapp.com
+  config.action_mailer.default_url_options = { :host => 'http://spamolacat-bloccit.herokuapp.com' }
+  
+
 end
