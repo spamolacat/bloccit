04deecdb2e1c662b3b49dd3555cf7fcbeb9d08f6
Matt MacDonald <spamolacat@yahoo.com>
2013-11-03 14:14:35 -0800

    Ajaxified comment destruction

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,8 +1,7 @@
 class CommentsController < ApplicationController
- 
   respond_to :html, :js
-
-    def create
+  
+  def create
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
     @comments = @post.comments
@@ -24,23 +23,22 @@ class CommentsController < ApplicationController
     end
   end
 
-
-    def destroy
+  def destroy
     @topic = Topic.find(params[:topic_id])
     @post = @topic.posts.find(params[:post_id])
 
     @comment = @post.comments.find(params[:id])
-
     authorize! :destroy, @comment, message: "You need to own the comment to delete it."
+
     if @comment.destroy
       flash[:notice] = "Comment was removed."
-      redirect_to [@topic, @post]
     else
       flash[:error] = "Comment couldn't be deleted. Try again."
-      redirect_to [@topic, @post]
     end
 
-    
-
+    respond_with(@comment) do |f|
+      f.html { redirect_to [@topic, @post] }
+    end
   end
+
 end
