d3b1e6a2d86c396b8fe4b4be4242c6f7b643182d
Matt MacDonald <spamolacat@yahoo.com>
2013-11-03 14:28:25 -0800

    Ajaxified comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,27 +1,27 @@
 class CommentsController < ApplicationController
   respond_to :html, :js
   
-  def create
-    @topic = Topic.find(params[:topic_id])
-    @post = @topic.posts.find(params[:post_id])
-    @comments = @post.comments
+def create
+  @topic = Topic.find(params[:topic_id])
+  @post = @topic.posts.find(params[:post_id])
+  @comments = @post.comments
 
-    @comment = current_user.comments.build(params[:comment])
-    @comment.post = @post
-    @new_comment = Comment.new
-    
-    authorize! :create, @comment, message: "You need be signed in to do that."
+  @comment = current_user.comments.build(params[:comment])
+  @comment.post = @post
+  @new_comment = Comment.new
 
-    if @comment.save
-      flash[:notice] = "Comment was created."
-    else
-      flash[:error] = "There was an error saving the comment. Please try again."
-    end
+  authorize! :create, @comment, message: "You need be signed in to do that."
 
-    respond_with(@comment) do |f|
-      f.html { redirect_to [@topic, @post] }
-    end
+  if @comment.save
+    flash[:notice] = "Comment was created."
+  else
+    flash[:error] = "There was an error saving the comment. Please try again."
   end
+
+  respond_with(@comment) do |f|
+    f.html { redirect_to [@topic, @post] }
+  end
+end
 
   def destroy
     @topic = Topic.find(params[:topic_id])
